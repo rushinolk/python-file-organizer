517c377c70db2e9a298d513b197b981c4322e6f7
rushinolk <arthurmachado7191@hotmail.com>
2025-10-30 17:21:14 -0300

    Mapeamento dinamico de extensão, Necessario manuntenção na verificação de arquivos com mesmo nome

--- main.py
+++ main.py
@@ -3,18 +3,44 @@ import shutil
 
 path_dir = "E:\\random_dir\\"
 
-path_documents = f"{path_dir}\\Documents\\"
+mapeamento_extensoes = {
+    ".txt":"Documents",
+    ".docx":"Documents",
+    ".pdf":"Documents",
+    ".png":"Imagens",
+    ".jpeg":"Imagens",
+    ".zip":"Zipados",
+    ".rar":"Zipados"
+}
 
-if os.path.exists(path_documents) is False:
-    os.mkdir(path_documents)
 
 for arquivo in os.listdir(path_dir):
 
-    if arquivo.endswith(".txt"):
-        path_arquivo_origem = os.path.join(path_dir,arquivo)
-        path_arquivo_destino = os.path.join(path_documents,arquivo)
+    path_completo = os.path.join(path_dir,arquivo)
 
-        if os.path.isfile(path_arquivo_origem):
-            shutil.move(path_arquivo_origem,path_arquivo_destino)
+    if os.path.isfile(path_completo):
+        extensao = os.path.splitext(arquivo)
+        nome_pasta = mapeamento_extensoes.get(extensao[1],"Outros")
+        pasta_destino = os.path.join(path_dir,nome_pasta)
+        arquivo_pasta_destino = os.path.join(pasta_destino,arquivo)
+
+        if os.path.exists(pasta_destino) is False:
+            os.mkdir(pasta_destino)
+
+        if os.path.exists(arquivo_pasta_destino) is True:
+            i = 1
+            arquivo_pasta_destino_rn = arquivo_pasta_destino + (f"({i})")
+            while True:
+                if os.path.exists(arquivo_pasta_destino_rn) is False:
+                    os.rename(path_completo,arquivo_pasta_destino_rn)
+                    break
+                else:
+                    i += 1
+        else:
+            shutil.move(path_completo,pasta_destino)
             print(f"Arquivo: {arquivo} \n Movido com sucesso")
+                    
+
+
+
 
